7e80f2b389d508e382427234ca66c7aec82809fb
Aryna <arina.savel97@gmail.com>
2026-07-18 20:54:50 +0700

    Polish Translation Mode layout, banner, controls, and shared components

--- script.js
+++ script.js
@@ -6995,7 +6995,7 @@ function updatePearls() {
 
     updateGameScoreCounter();
 
-    if (currentGameMode === "unscramble" || currentGameMode === "picture") {
+    if (currentGameMode === "unscramble" || currentGameMode === "picture" || currentGameMode === "translation") {
         for (let i = 0; i < gameCards.length; i++) {
             const spacer = document.createElement("span");
             spacer.className = "pearl-empty";
